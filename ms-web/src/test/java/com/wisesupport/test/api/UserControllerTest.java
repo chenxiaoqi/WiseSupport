80bd16b44e1f280dd72b57ac8c1f745948ab3ab4
cxqpfan@163.com <cxqpfan@163.com>
2019-03-03 07:15:38 +0800

    add some tests

--- ms-web/src/test/java/com/wisesupport/test/api/UserControllerTest.java
+++ ms-web/src/test/java/com/wisesupport/test/api/UserControllerTest.java
@@ -1,19 +1,34 @@
 package com.wisesupport.test.api;
 
+import com.wisesupport.Swagger2Configuration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Author chenxiaoqi on 2019-03-02.
  */
-@JdbcTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@ImportAutoConfiguration(exclude = {Swagger2Configuration.class})
 public class UserControllerTest {
 
-    @Test
-    public void testAdd() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    @WithMockUser("ROLE_ADMIN")
+    public void testAdd() throws Exception {
+        this.mockMvc.perform(get("/user/user_list")).andExpect(status().isFound());
     }
 }
