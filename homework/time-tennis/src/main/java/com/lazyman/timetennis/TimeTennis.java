f97998f110bff44c22e9a68b84a3787e0ecfa0d6
ryan <cxqpfan@163.com>
2020-06-13 13:43:43 +0800

    修改密码

--- homework/time-tennis/src/main/java/com/lazyman/timetennis/TimeTennis.java
+++ homework/time-tennis/src/main/java/com/lazyman/timetennis/TimeTennis.java
@@ -4,11 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"com.lazyman.timetennis", "com.wisesupport.commons"})
 @ImportResource("classpath:/spring/applicationContext.xml")
+@PropertySource("classpath:password.properties")
 @ServletComponentScan(basePackageClasses = TimeTennis.class)
 @EnableScheduling
 @EnableTransactionManagement(proxyTargetClass = true)
