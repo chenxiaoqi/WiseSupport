f736b22adc95f85da371e26bc10b09cbae24f4f3
ryan <cxqpfan@163.com>
2020-09-17 21:02:21 +0800

    membership card

--- homework/time-tennis/src/script/update.sql
+++ homework/time-tennis/src/script/update.sql
@@ -1,11 +1,17 @@
-alter table tt_booking
-    add arena_id int default 2 not null after open_id;
+drop index idx_open_id on membership_card;
 
-alter table tt_booking
-    add court_id int default 7 not null after arena_id;
+create unique index idx_open_id_meta_id
+    on membership_card (open_id, meta_id);
 
-create index idx_date_arena_id
-    on tt_booking (arena_id, date);
+alter table membership_card
+    modify balance int not null after meta_id;
 
-create index idx_open_id_date
-    on tt_booking (open_id, date);
+alter table membership_card
+    add expire_date date not null after balance;
+
+alter table membership_card
+    drop column status;
+
+create index idx_meta_id
+    on membership_card (meta_id);
+
