9895049e578329245e25f202a5e0037357727a18
ryan <cxqpfan@163.com>
2020-09-21 21:44:08 +0800

    refactor

--- homework/time-tennis/src/main/java/com/lazyman/timetennis/menbership/MembershipCardManageController.java
+++ homework/time-tennis/src/main/java/com/lazyman/timetennis/menbership/MembershipCardManageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/mine/mc")
+@RequestMapping("/manage/mc")
 public class MembershipCardManageController {
     private MembershipCardDao mcDao;
 
